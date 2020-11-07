niclashedam/eid-hermes-qemu

commit 9d38cf97af58349953d0615ed422ac7126a1d477

Implement DMA on Hermes
Using the xdma driver[1], we can now send DMA commands to Hermes. It has
been tested with this program[2] and works even if we increase the
buffer size (so it uses multiple descriptors, including when they cross
a 4K address boundary).

[1]: https://github.com/aws/aws-fpga/tree/master/sdk/linux_kernel_drivers/xdma
[2]: https://github.com/aws/aws-fpga/tree/master/sdk/linux_kernel_drivers/xdma#quick-example

Signed-off-by: Martin Oliveira <martin.oliveira@eideticom.com>

hw/misc/hermes.c
@@ -138,6 +138,9 @@ struct hermes_bar2_sgdma_reg {
 struct hermes_bar2_dir_reg {
     struct hermes_bar2_engine_reg channel;
     struct hermes_bar2_sgdma_reg sgdma;
+    void (*perform_dma)(HermesState *hermes, hwaddr src_addr, hwaddr dst_addr,
+                        dma_addr_t len);
+    int irq_vector;
 };
 
 struct hermes_bar2 {
@@ -155,6 +158,110 @@ struct HermesState {
 
     struct hermes_bar2 bar2;
 };
+
+struct hermes_dma_desc {
+    uint32_t ctrl;
+    uint32_t len;
+    hwaddr src_addr;
+    hwaddr dst_addr;
+    hwaddr nxt_addr;
+};
+
+static int hermes_execute_descs(HermesState *hermes,
+                                struct hermes_bar2_dir_reg *dir,
+                                hwaddr *desc_addr, uint8_t *num_desc)
+{
+    struct hermes_dma_desc *desc;
+    int i;
+
+    trace_hermes_dma_num_adj(*num_desc - 1);
+    desc = g_new(struct hermes_dma_desc, *num_desc);
+    if (!desc) {
+        fprintf(stderr, "[Hermes] Failed to alloc memory for DMA descriptor\n");
+        return -1;
+    }
+
+    /* Read DMA descriptors */
+    pci_dma_read(&hermes->pdev, *desc_addr, desc, *num_desc * sizeof(*desc));
+
+    /* DMA all descriptors in this block */
+    for (i = 0; i < *num_desc; i++) {
+        trace_hermes_dma_desc(desc[i].ctrl, desc[i].len, desc[i].src_addr,
+                              desc[i].dst_addr, desc[i].nxt_addr);
+        dir->perform_dma(hermes, desc[i].src_addr, desc[i].dst_addr,
+                         desc[i].len);
+    }
+
+    *desc_addr = desc[*num_desc - 1].nxt_addr;
+    *num_desc = (desc[*num_desc - 1].ctrl >> 8) & 0x3F;
+
+    g_free(desc);
+
+    return i;
+}
+
+static void do_dma(HermesState *hermes, struct hermes_bar2_dir_reg *dir)
+{
+    struct hermes_bar2 *bar2 = &hermes->bar2;
+    struct hermes_bar2_irq_reg *irq = &bar2->irq;
+    hwaddr desc_addr;
+    uint8_t num_desc;
+    int ret;
+
+    /* Set engine as busy */
+    atomic_or(&dir->channel.status, 0x1);
+
+    /* Reset number of completed descriptors */
+    dir->channel.cmp_desc_count = 0;
+
+    /*
+     * There is always at least one descriptor, plus the adjacent ones (which
+     * could be 0). Only bits 5:0 of the register are defined
+     */
+    num_desc = 1 + (dir->sgdma.desc_num_adj & 0x3F);
+    desc_addr = dir->sgdma.desc_addr;
+    while (desc_addr) {
+        ret = hermes_execute_descs(hermes, dir, &desc_addr, &num_desc);
+        if (ret < 0) {
+            break;
+        }
+
+        /* Update number of completed descriptors */
+        dir->channel.cmp_desc_count += ret;
+    }
+
+    /* Set engine as not busy */
+    atomic_and(&dir->channel.status, ~1);
+
+    if (irq->chnl_inter_enable_mask & (dir->irq_vector + 1)) {
+        /* Set interrupt source */
+        irq->chnl_inter_request =  irq->chnl_inter_enable_mask &
+                                   (dir->irq_vector + 1);
+
+        /*
+         * Send interrupt. Since we currently have only one channel for H2C and
+         * one for C2H, we have that IRQ 0 is H2C and IRQ 1 is C2H
+         */
+        trace_hermes_msix_notify(dir->irq_vector);
+        msix_notify(PCI_DEVICE(hermes), dir->irq_vector);
+    }
+}
+
+static void hermes_h2c_dma(HermesState *hermes, hwaddr src_addr,
+                           hwaddr dst_addr, dma_addr_t len)
+{
+    void *bar4_base = memory_region_get_ram_ptr(&hermes->bar4_mem_reg);
+    trace_hermes_dma("H2C", src_addr, dst_addr, len);
+    pci_dma_read(&hermes->pdev, src_addr, bar4_base + dst_addr, len);
+}
+
+static void hermes_c2h_dma(HermesState *hermes, hwaddr src_addr,
+                           hwaddr dst_addr, dma_addr_t len)
+{
+    void *bar4_base = memory_region_get_ram_ptr(&hermes->bar4_mem_reg);
+    trace_hermes_dma("C2H", src_addr, dst_addr, len);
+    pci_dma_write(&hermes->pdev, dst_addr, bar4_base + src_addr, len);
+}
 
 static const struct hermes_bar2 bar2_init = {
     .h2c = {
@@ -176,6 +283,8 @@ static const struct hermes_bar2 bar2_init = {
              * .inter_enable_mask = 0x00F83E1E,
              */
         },
+        .perform_dma = hermes_h2c_dma,
+        .irq_vector = 0,
     },
     .c2h = {
         .channel = {
@@ -192,6 +301,8 @@ static const struct hermes_bar2 bar2_init = {
              * .inter_enable_mask = 0x00F83E1E,
              */
         },
+        .perform_dma = hermes_c2h_dma,
+        .irq_vector = 1,
     },
     .irq = {
         .identifier = HERMES_ID_CORE | HERMES_ID_IRQ | HERMES_ID_VER_20164,
@@ -326,7 +437,7 @@ static uint64_t hermes_bar2_engine_write(HermesState *hermes,
     case 0x04:
         reg->control = 0x0FFFFE7F & val;
         if (reg->control & 0x1) {
-            /* do_dma */
+            do_dma(hermes, dir);
         }
         break;
     case 0x08:

hw/misc/trace-events
@@ -221,3 +221,7 @@ pca955x_gpio_change(const char *description, unsigned id, unsigned prev_state, u
 # hermes.c
 hermes_bar2_read(unsigned int size, uint64_t addr, uint64_t value) "Hermes BAR2 read sz:%u addr:0x%"PRIx64" data:0x%"PRIx64
 hermes_bar2_write(unsigned int size, uint64_t addr, uint64_t value) "Hermes BAR2 write sz:%u addr:0x%"PRIx64" data:0x%"PRIx64
+hermes_dma(const char *channel, uint64_t src, uint64_t dst, uint64_t len) "Hermes DMA %s src: 0x%" PRIx64" dst: 0x%" PRIx64 " len: %lu"
+hermes_dma_desc(uint32_t ctrl, uint32_t len, uint64_t src_addr, uint64_t dst_addr, uint64_t nxt_addr) "Hermes DMA Desc: ctrl: 0x%"PRIx32" len: 0x%"PRIx32" src_addr: 0x%"PRIx64" dst_addr: 0x%"PRIx64" nxt_addr: 0x%"PRIx64"
+hermes_dma_num_adj(unsigned num_adj) "Hermes DMA: num adj: %u"
+hermes_msix_notify(unsigned vector) "Hermes MSI-X notify %u"

commit 8393ce6584c4a89f290f23fc8dae2e05471ddc8f

Compute number of descriptors in next DMA block correctly
DMA descriptors are processed in blocks, and the last descriptor of a
block points to the address and number of *adjacent* descriptors in the
next block (Nxt_adj and Nxt_addr on Table 5 of PG195[1]).

However, we were not computing the number of descriptors in subsequent
blocks properly, because we weren't adding 1 to account for the first
descriptor (it is valid to have nxt_adj == 0).

This has been tested with a 32MB XDMA transfer.

Fixes: 9d38cf97af58 ("Implement DMA on Hermes")

[1]: https://www.xilinx.com/support/documentation/ip_documentation/xdma/v4_1/pg195-pcie-dma.pdf

Signed-off-by: Martin Oliveira <martin.oliveira@eideticom.com>

hw/misc/hermes.c
@@ -200,7 +200,7 @@ static int hermes_execute_descs(HermesState *hermes,
     }
 
     *desc_addr = desc[*num_desc - 1].nxt_addr;
-    *num_desc = (desc[*num_desc - 1].ctrl >> 8) & 0x3F;
+    *num_desc = 1 + ((desc[*num_desc - 1].ctrl >> 8) & 0x3F);
 
     g_free(desc);
 